a1a6c83550c537c2e189f261d254edc0eebbd4e8
ItsRedXO <lopezdavid689@yahoo.com>
2026-08-21 00:40:40 -0700

    test: cover safe ranged projectile launch and aim points

--- Assets/Tests/Editor/EnemyProjectileTests.cs
+++ Assets/Tests/Editor/EnemyProjectileTests.cs
@@ -29,4 +29,20 @@ public class EnemyProjectileTests
             Object.DestroyImmediate(attacker);
         }
     }
+
+    [Test]
+    public void LaunchPositionIsRaisedAboveFloor()
+    {
+        Vector3 launchPosition = EnemyProjectile.GetLaunchPosition(Vector3.zero);
+
+        Assert.That(launchPosition, Is.EqualTo(new Vector3(0f, 0.75f, 0f)));
+    }
+
+    [Test]
+    public void AimPointTargetsUpperBodyInsteadOfFeet()
+    {
+        Vector3 aimPoint = EnemyProjectile.GetAimPoint(Vector3.zero);
+
+        Assert.That(aimPoint, Is.EqualTo(new Vector3(0f, 1f, 0f)));
+    }
 }
