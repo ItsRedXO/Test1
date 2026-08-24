9546269eb79b053a0318306907691796c490d662
ItsRedXO <lopezdavid689@yahoo.com>
2026-08-24 11:40:45 -0700

    Add encounter system foundation

---
+++ Assets/Scripts/Encounters/EncounterZone.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+namespace ActionRPG.Encounters
+{
+    [RequireComponent(typeof(Collider))]
+    public class EncounterZone : MonoBehaviour
+    {
+        [SerializeField] private EncounterManager encounterManager;
+        [SerializeField] private bool triggerOnlyOnce = true;
+
+        private bool hasTriggered;
+
+        private void Reset()
+        {
+            Collider zoneCollider = GetComponent<Collider>();
+            if (zoneCollider != null) zoneCollider.isTrigger = true;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (hasTriggered && triggerOnlyOnce) return;
+            if (!other.CompareTag("Player")) return;
+
+            if (encounterManager == null)
+            {
+                encounterManager = GetComponentInParent<EncounterManager>();
+            }
+
+            if (encounterManager == null)
+            {
+                Debug.LogWarning("[Encounter] EncounterZone has no EncounterManager assigned.", this);
+                return;
+            }
+
+            if (encounterManager.TryStartEncounter())
+            {
+                hasTriggered = true;
+            }
+        }
+    }
+}
